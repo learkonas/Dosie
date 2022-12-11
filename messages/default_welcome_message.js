f264dce21bb50b153213f5fec8c2a39af8ebd51f
learkonas <leo.nasskau@gmail.com>
2022-12-11 22:52:54 +0000

    removed boilerplate files. will add AAA later

--- messages/default_welcome_message.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  "welcome_message": {
-    "message_data": {
-      "text": "Select a feature to demo.",
-      "quick_reply": require('./fragment_demo_features_options')
-    }
-  }
-}
