4ea99b7495ebd49e27c274539d1355a40ca4159c
learkonas <leo.nasskau@gmail.com>
2022-12-11 22:54:38 +0000

    adjusted index.js after removing boilerplate

--- index.js
+++ index.js
@@ -3,9 +3,7 @@ const {readFileSync} = require('fs');
 const handlebars = require('handlebars');
 
 var bodyParser = require('body-parser')
-var security = require('./security')
-var message_processor = require('./message-processor')
-var twitter = require('./twitter')
+
 
 const app = express();
 // Serve the files in /assets at the URI /assets.
@@ -44,36 +42,9 @@ app.use(express.static(__dirname + '/public'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.set('views', __dirname + '/views')
+app.set('views', __dirname + '/GCR/views')
 app.set('view engine', 'ejs')
 
-
-
-// Receives challenge response check (CRC)
-app.get('/webhooks/twitter', function(request, response) {
-
-  var crc_token = request.query.crc_token
-
-  if (crc_token) {
-    var hash = security.get_challenge_response(crc_token, twitter.oauth.consumer_secret)
-
-    response.status(200);
-    response.send({
-      response_token: 'sha256=' + hash
-    })
-  } else {
-    response.status(400);
-    response.send('Error: crc_token missing from request.')
-  }
-})
-
-// Receives DM events
-app.post('/webhooks/twitter', function(request, response) {
-
-  message_processor.process(request.body) // replace this with your own bot logic
-
-  response.send('200 OK')
-})
 
 var PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
